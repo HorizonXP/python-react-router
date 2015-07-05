12c89b33c51b732f9747fea95eb5879f92aea1c7
Xitij Ritesh Patel <ritesh@buildscience.com>
2015-07-05 04:47:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
-import react
+import react_router
 
 setup(
-    name='react',
-    version=react.__version__,
-    packages=['react'],
+    name='react_router',
+    version=react_router.__version__,
+    packages=['react_router'],
     install_requires=[
         'webpack==4.1.1',
         'optional-django==0.3.0',
     ],
-    description='Server-side rendering, client-side mounting, JSX translation, and component bundling',
-    long_description='Documentation at https://github.com/markfinger/python-react',
-    author='Mark Finger',
-    author_email='markfinger@gmail.com',
-    url='https://github.com/markfinger/python-react',
+    description='Server-side rendering, client-side mounting, JSX translation, and component bundling with react-router.',
+    long_description='Documentation at https://github.com/HorizonXP/python-react-router',
+    author='Xitij Ritesh Patel',
+    author_email='github@xitijpatel.com',
+    url='https://github.com/HorizonXP/python-react-router',
 )
